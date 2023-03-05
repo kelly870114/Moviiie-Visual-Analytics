6256779454705d74e5891fb1b49215fad77aa575
Ginny Huang <kelly87011431@gmail.com>
2023-03-04 22:16:18 -0800

    revise bubble chart

--- src/components/charts/BubbleChart.js
+++ src/components/charts/BubbleChart.js
@@ -1,91 +1,50 @@
 import React from "react";
 import ReactApexChart from "react-apexcharts";
 function generateData(baseval, count, yrange) {
-    var i = 0;
-    var series = [];
-    while (i < count) {
-      var x = Math.floor(Math.random() * (750 - 1 + 1)) + 1;;
-      var y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
-      var z = Math.floor(Math.random() * (75 - 15 + 1)) + 15;
-  
-      series.push([x, y, z]);
-      baseval += 86400000;
-      i++;
-    }
-    return series;
+ var i = 0;
+ var series = [];
+ while (i < count) {
+  var x = Math.floor(Math.random() * (750 - 1 + 1)) + 1;
+  var y =
+   Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
+  var z = Math.floor(Math.random() * (75 - 15 + 1)) + 15;
+
+  series.push([x, y, z]);
+  baseval += 86400000;
+  i++;
+ }
+ return series;
 }
 class BubbleChart extends React.Component {
-    constructor(props) {
-      super(props);
+ constructor(props) {
+  super(props);
 
-      this.state = {
-        chartData:[],
-        chartOption:[],
-        series: [{
-          name: 'Bubble1',
-          data: generateData(new Date('11 Feb 2017 GMT').getTime(), 20, {
-            min: 10,
-            max: 60
-          })
-        },
-        {
-          name: 'Bubble2',
-          data: generateData(new Date('11 Feb 2017 GMT').getTime(), 20, {
-            min: 10,
-            max: 60
-          })
-        },
-        {
-          name: 'Bubble3',
-          data: generateData(new Date('11 Feb 2017 GMT').getTime(), 20, {
-            min: 10,
-            max: 60
-          })
-        },
-        {
-          name: 'Bubble4',
-          data: generateData(new Date('11 Feb 2017 GMT').getTime(), 20, {
-            min: 10,
-            max: 60
-          })
-        }],
-        options: {
-          chart: {
-              height: 350,
-              type: 'bubble',
-          },
-          dataLabels: {
-              enabled: false
-          },
-          fill: {
-              opacity: 0.8
-          },
-          title: {
-              text: 'Simple Bubble Chart'
-          },
-          xaxis: {
-              tickAmount: 12,
-              type: 'category',
-          },
-          yaxis: {
-              max: 70
-          }
-        },
-      
-      
-      };
-    }
+  this.state = {
+   chartData: [],
+   chartOptions: {},
+  };
+ }
 
-  
+ componentDidMount() {
+  this.setState({
+   chartData: this.props.chartData,
+   chartOptions: this.props.chartOptions,
+  });
+ }
 
-    render() {
-      return (
-        <div id="chart">
-            <ReactApexChart options={this.state.options} series={this.state.series} type="bubble" height={350} />
-        </div>
-
-      );
-    }
+ render() {
+  return (
+   <div id="chart">
+    <ReactApexChart
+     options={this.state.chartOptions}
+     series={this.state.chartData}
+     type="bubble"
+     height={350}
+     width={700}
+    />
+   </div>
+  );
+ }
 }
 
 export default BubbleChart;
